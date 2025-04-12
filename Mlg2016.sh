5ffbb1c448242b0bdec1b6853e51d1e457defc13
Websittoo <kysnglyw@gmail.com>
2025-04-12 10:26:04 +0200

    ultra update
    Animations, tiktok etc.!

--- Mlg2016.sh
+++ Mlg2016.sh
@@ -1,6 +1,15 @@
 cat > ~/.bashrc << 'EOF'
 clear
 
+# Animacja noscope loading
+for frame in ">" ">>" ">>>" ">>>>" ">>>>>" ">>>>>>" "NOSCOPE READY"; do
+    echo -ne "\r\e[1;5;33mLoading $frame\e[0m"
+    sleep 0.15
+done
+echo ""
+sleep 0.3
+
+# ASCII BANNER
 echo -e "\e[1;35m
 ███████╗███╗   ███╗ ██████╗      ███╗   ███╗██╗     ██████╗ 
 ██╔════╝████╗ ████║██╔═══██╗     ████╗ ████║██║     ██╔══██╗
@@ -12,8 +21,9 @@ echo -e "\e[1;35m
 
 echo -e "\e[1;36m███████╗\e[1;31m M L G   \e[1;33m N M G \e[1;32m 2 0 1 6 \e[0m"
 echo -e "\e[1;5;33m420 NOSCOPE ACTIVATED - FAZE UP\e[0m"
-
 echo ""
+
+# ASCII ART
 echo -e "\e[1;32m     (  )  (   ) )"
 echo -e "      ) (   )  (  )"
 echo -e "      ( )  (    ) )         \e[1;31mDORITOS"
@@ -28,10 +38,12 @@ echo -e " \\_____________________/"
 echo ""
 
 echo -e "\e[1;5;35m[+] SYSTEM READY FOR 360 NOSCOPE [+]\e[0m"
+echo -e "\e[1;36mAuthor: TikTok: ApkModerZNJ\e[0m"
 echo ""
 
+# MLG Prompt
 PS1="\e[1;35m[MLG 2016] > \e[0m"
 EOF
 
-# Reload bashrc instantly
+# Apply changes
 source ~/.bashrc
