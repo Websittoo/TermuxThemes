7b7c7fe7a3cf21c4c4a4e5fcb9a9b45d3153c5dd
Websittoo <kysnglyw@gmail.com>
2025-04-12 09:40:44 +0200

    Create owntheme.sh

---
+++ owntheme.sh
@@ -0,0 +1,8 @@
+echo -e "\e[31m$(figlet Commands:)\e[0m"
+echo echo - normal text
+echo figlet - ascii
+echo colors u csn add using the echo -e \ - u can use google
+sleep 5
+echo save whit ctrl x and next Y and enter x2
+sleep 4
+nano ~/.bashrc
