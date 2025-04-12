102696879d1f68a11e274e784adc1ff180c75565
Websittoo <kysnglyw@gmail.com>
2025-04-12 10:17:14 +0200

    Update Pkgs.sh

--- Pkgs.sh
+++ Pkgs.sh
@@ -1,2 +1,9 @@
 pkg install figlet
-figlet ready!
+figlet figlet is ready!
+pkg install python
+figlet python is ready!
+echo -e "\e[32m$(figlet some mods!)\e[0m"
+pkg install tree
+figlet tree is ready!
+pkg install proot-distro
+figlet proot distro ready!
